bb39dfdd6ed130ec0e51ce2a1f4ad29f1bfad206
Denislav883 <yanev.denislav@gmail.com>
2025-11-14 01:41:04 +0200

    Refactor UI into reusable React components
    Split the main UI elements into separate React components: Header, Footer, Search, Table, and Pagination. Updated App.jsx to use these components and cleaned up resources.jsx to remove duplicated markup, improving maintainability and modularity.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,28 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Pagination from "./components/Pagination";
+import Search from "./components/Search";
+import Table from "./components/Table";
 
 function App() {
 
     return (
         <div>
-            <h1>Hello!</h1>
+            <Header />
+
+            <main className="main">
+                <section className="card users-container">
+                    <Search />
+
+                    <Table />
+
+                    <button className="btn-add btn">Add new user</button>
+
+                    <Pagination />
+                </section>
+            </main>
+
+            <Footer />
         </div>
     );
 }
